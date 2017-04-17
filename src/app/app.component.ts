e9a869c01424c0b08b0218f7247e76f3ddb68665
ngkhnam <ngkhnam@gmail.com>
2017-04-17 13:59:39 +0700

    update using multi selection and custom selection

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, ViewChild, OnInit } from '@angular/core';
-import { NNTreeComponent } from 'nn-angular-tree';
+import { NNTreeComponent, NNTreeNode } from 'nn-angular-tree';
 
 @Component({
   selector: 'app-root',
@@ -25,20 +25,20 @@ export class AppComponent implements OnInit {
       var child = { label: "child " + i, children: [], open: false };
       tree.children.push(child);
       var hasChildren = this.getRandomInt(1, 6);
-      if(hasChildren > 3){
+      if (hasChildren > 3) {
         child.children = this.generateChildTree(20, child.label);
       }
-      
+
     }
     return tree;
   }
 
-  generateChildTree(number: number, name: string){
+  generateChildTree(number: number, name: string) {
     let length = this.getRandomInt(1, number);
-    let children  = [];
+    let children = [];
     for (let i = 0; i < length; i++) {
       var child = { label: "child of " + name + " " + i, children: null, open: false };
-      children.push(child);      
+      children.push(child);
     }
     return children;
   }
@@ -46,14 +46,14 @@ export class AppComponent implements OnInit {
   generateLazyLoadingTree(number: number): any {
     let tree = { label: "root", children: [], open: true };
     for (let i = 0; i < number; i++) {
-      var child = { label: "child " + i, children: [], open: false, lazyLoading: true};
+      var child = { label: "child " + i, children: [], open: false, lazyLoading: true };
       tree.children.push(child);
     }
     return tree;
   }
-  
+
   onOpen(node: any) {
-    if(!node.children)
+    if (!node.children)
       node.children = [];
     if (!node.children.length) {
       this.lazyLoadingTree.showLoading(node);
@@ -80,5 +80,16 @@ export class AppComponent implements OnInit {
 
   getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+
+  onChangeCheckbox(event: Event) {
+    console.log("change");
+  }
+
+  canSelectNode(node: NNTreeNode) {
+    if (node.children && node.children.length > 0)
+      return false;
+    else
+      return true;
   }
 }
